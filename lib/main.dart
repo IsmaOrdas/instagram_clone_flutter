3a60471d845d7f6f1bda972ea11f9e0824d3ec9a
Ismael Ordás Carro <ismael.ordas@narrativa.com>
2022-12-06 18:16:08 +0100

    Setup Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone_flutter/responsive/responsive_layout.dart';
 import 'package:instagram_clone_flutter/utils/colors.dart';
-import 'package:instagram_clone_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone_flutter/responsive/web_screen_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: 'AIzaSyBdqmNEfi6FE43TR8GyteqsEhjWVbOpt7I',
+            appId: '1:198360634208:web:20adda569921eeb3960405',
+            messagingSenderId: '198360634208',
+            projectId: 'instagram-clone-flutter-5c8e2',
+            storageBucket: 'instagram-clone-flutter-5c8e2.appspot.com'));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
